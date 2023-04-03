dd30ed73d2d547bd83276543fac1b39c98c280fc
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-04-03 12:27:29 -0300

    Update Media.js

--- Atividade6/Media.js
+++ Atividade6/Media.js
@@ -1,10 +1,9 @@
-    
-    
-   
-    nota1 = prompt("Digite a primeira nota");
-    nota2 = prompt("Digite a segunda nota"); 
-    nota3 = prompt("Digite a terceira nota"); 
+nome = prompt("Digite o nome do aluno");
 
-    var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
+nota1 = prompt("Digite a primeira nota");
+nota2 = prompt("Digite a segunda nota"); 
+nota3 = prompt("Digite a terceira nota"); 
 
-    alert("Sua média é " + media); 
+var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
+
+alert("A média do aluno " + nome + " é " + media); 
