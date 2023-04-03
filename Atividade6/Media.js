5ddb2492cb104c858b6901fdd2dd35030da3b3de
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-04-03 12:23:20 -0300

    Add files via upload

---
+++ Atividade6/Media.js
@@ -0,0 +1,10 @@
+    
+    
+   
+    nota1 = prompt("Digite a primeira nota");
+    nota2 = prompt("Digite a segunda nota"); 
+    nota3 = prompt("Digite a terceira nota"); 
+
+    var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
+
+    alert("Sua média é " + media); 
---
+++ Atividade6/Operacoes.js
@@ -0,0 +1,14 @@
+var num1 = parseFloat(prompt("Digite o numero 1"));
+var num2 = parseFloat(prompt("Digite o numero 2"));
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var prod = num1 * num2;
+var div = num1 / num2;
+var res = num1 % num2;
+
+alert("soma: " + sum + "\n" + 
+    "subtração: " + sub + "\n" +
+    "multiplicão: " + prod + "\n" +
+    "divisão: " + div + "\n" +
+    "resto: " + res + "\n")
