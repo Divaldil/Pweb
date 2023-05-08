cc856fe6d950de3ad46d5e8e27ed24b7ebae756b
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-05-08 11:59:57 -0300

    Add files via upload

---
+++ Atividade12/eventos.js
@@ -0,0 +1,20 @@
+function abrir(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://www.pngitem.com/pimgs/m/38-385235_window-royalty-free-clip-art-open-windows-clipart.png";
+    var texto = document.getElementById("456");
+    texto.innerHTML="Janela Aberta";
+}
+
+function fechar(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://cdn.vectorstock.com/i/preview-1x/72/12/window-cartoon-symbol-icon-design-vector-24007212.jpg";
+    var texto = document.getElementById("456");
+    texto.innerHTML="Janela Fechada";
+}
+
+function quebrar(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://e7.pngegg.com/pngimages/1018/356/png-clipart-old-windows-window-broken-windows.png";
+    var texto = document.getElementById("456");
+    texto.innerHTML="Janela Quebrada";
+}
