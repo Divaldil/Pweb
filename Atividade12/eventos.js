3f55d70863634087d30e5b91d3b2562a559d4e5e
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-05-08 11:34:21 -0300

    Add files via upload

---
+++ Atividade12/eventos.js
@@ -0,0 +1,14 @@
+function abrir(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://www.pngitem.com/pimgs/m/38-385235_window-royalty-free-clip-art-open-windows-clipart.png";
+}
+
+function fechar(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://cdn.vectorstock.com/i/preview-1x/72/12/window-cartoon-symbol-icon-design-vector-24007212.jpg";
+}
+
+function quebrar(){
+    var imagem = document.getElementById("123");
+    imagem.src = "https://e7.pngegg.com/pngimages/1018/356/png-clipart-old-windows-window-broken-windows.png";
+}
