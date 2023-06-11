621232fff52df77aef598fa03a53a2befbca25f7
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-06-10 23:42:54 -0300

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,34 @@
+function mudar(){
+    var imagem = document.getElementById("serralheiro");
+    imagem.src = "https://static.wixstatic.com/media/1f7d33_a3008effeb1f43278aaf342ed7faee93~mv2.png/v1/fill/w_1000,h_668,al_c,q_90/1f7d33_a3008effeb1f43278aaf342ed7faee93~mv2.png"
+    var opacity = 0.5;
+    imagem.style.opacity = opacity;
+}
+function prox(){
+    var imagem = document.getElementById("serralheiro");
+    imagem.src = "https://casaserralheiro.com.br/wp-content/uploads/2022/01/casadoserralheiro-blog-equipamentos-para-serralheria-epis-indispensaveis.png"
+    var opacity = 1;
+    imagem.style.opacity = opacity;
+}
+
+function escurecerG(){
+    var icon = document.getElementById("logogthb");
+    var opacity = 1;
+    icon.style.opacity = opacity;
+}
+function clarearG(){
+    var icon = document.getElementById("logogthb");
+    var opacity = 0.6;
+    icon.style.opacity = opacity;
+}
+
+function escurecerL(){
+    var icon = document.getElementById("logolkdn");
+    var opacity = 1;
+    icon.style.opacity = opacity;
+}
+function clarearL(){
+    var icon = document.getElementById("logolkdn");
+    var opacity = 0.6;
+    icon.style.opacity = opacity;
+}
